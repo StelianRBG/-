710a3b09d3c5435bf9640e9cc2451ca4d5618be0
StelianRBG <steliantodorichkov03@gmail.com>
2021-01-21 19:32:56 +0200

    Start of project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+@app.route("/signin")
+def log_page():
+    return render_template("login.html")
+
+@app.route("/signup")
+def reg_page():
+    return render_template("registration.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
