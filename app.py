529e9e9facfe6ca978101654e0338392198d90f8
StelianRBG <steliantodorichkov03@gmail.com>
2021-01-24 22:33:38 +0200

    login 85%

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
 import hashlib
 
 app = Flask(__name__)
@@ -9,19 +8,39 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, nullable = False)
-    password = db.Column(db.String, nullable = False)
+    username = db.Column(db.String, unique=True, nullable = False)
+    password = db.Column(db.String, unique=True, nullable = False)
+
+class Logged(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique=True, nullable = False)
+
 @app.route("/")
 def main():
     return render_template("home.html")
 
 @app.route("/logged")
 def logged_page():
-    return render_template("logged.html")
+    user = Logged.query.order_by(Logged.id.desc()).first()
+    return render_template("logged.html", profile=user.username)
 
-@app.route("/signin")
+@app.route("/signin", methods=['GET', 'POST'])
 def log_page():
-    return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        username = request.form['username']
+        password = (request.form['password'])
+        password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        user = User.query.filter_by(username = username, password = password).first()
+        if user is None:
+            return render_template("login.html", mesage="Try agai! This profile is not exists!")
+        else:
+            logged = Logged(username=username)
+            db.session.add(logged)
+            return redirect('/logged')
+
+    
 
 @app.route("/signup", methods=['GET', 'POST'])
 def reg_page():
@@ -35,8 +54,10 @@ def reg_page():
             password = hashlib.sha256(password.encode('utf-8')).hexdigest()
             record = User(username=username, password=password)
             db.session.add(record)
+            logged = Logged(username=username)
+            db.session.add(logged)
             db.session.commit()
-            return render_template("logged.html", profile=username)
+            return redirect('/logged')
         else:
             return render_template("registration.html", mesage="Try agai! This username is already used!")
 
